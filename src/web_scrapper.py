15e88cae3951a2e16125728bad1221eaa29297c7
0nmyhead <jg020135@naver.com>
2022-11-12 15:15:13 +0900

    scrapping href failed

--- src/web_scrapper.py
+++ src/web_scrapper.py
@@ -8,8 +8,22 @@ def get_page(page_url):
     """
 
     page = requests.get(page_url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    page.raise_for_status()
 
-    return soup, page
+    soup = BeautifulSoup(page.text, 'html.parser')
+    
+    items = soup.select("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li")
+    pagelist = []
+    
+    for item in items:
+        
+        pagenum = item.select_one("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div > a")["href"]
+        print(pagenum)
+    
+    
+    return 0
+    
 
-url = 'https://www.wanted.co.kr/wdlist/518/655?country=kr&job_sort=company.response_rate_order&years=-1&locations=all'
+url = 'https://www.wanted.co.kr/wdlist/518/655?country=kr&job_sort=job.latest_order&years=-1&locations=all'
+
+plist = get_page(url)
