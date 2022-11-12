3da969725fb3e188bc2142681ca17bd5669c3f06
0nmyhead <jg020135@naver.com>
2022-11-12 19:42:24 +0900

    db authentication

--- src/firebase_conn.py
+++ src/firebase_conn.py
@@ -1,9 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import WantedScrapper
 
 
-cred = credentials.Certificate('mykey.json')
+cred = credentials.Certificate('conductive-coil-351110-firebase-adminsdk-1is5w-ca0cdf61e0.json')
 firebase_admin.initialize_app(cred, {
   'projectId': 'conductive-coil-351110',
 })
