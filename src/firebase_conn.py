2ec2c3f166362a6f4f5d971e10cab816d98ae388
0nmyhead <jg020135@naver.com>
2022-11-12 23:54:22 +0900

    upload scrapped data to firestore test passed - firebase_conn.py

--- src/firebase_conn.py
+++ src/firebase_conn.py
@@ -4,11 +4,6 @@ from firebase_admin import firestore
 from WantedScrapper import WantedScrapper
 
 
-# cred = credentials.Certificate('conductive-coil-351110-firebase-adminsdk-1is5w-ca0cdf61e0.json')
-# firebase_admin.initialize_app(cred, {
-#   'projectId': 'conductive-coil-351110',
-# })
-
 scrapper = WantedScrapper()
 pagelist = scrapper.get_pagelist(scrapper.url)
 clist = []
@@ -17,4 +12,14 @@ for i in pagelist:
     
     clist.append(scrapper.get_info(i))
 
-# db = firestore.client()
+cred = credentials.Certificate('conductive-coil-351110-firebase-adminsdk-1is5w-ca0cdf61e0.json')
+firebase_admin.initialize_app(cred, {
+  'projectId': 'conductive-coil-351110',
+})
+
+
+db = firestore.client()
+
+for data in clist:
+    
+    db.collection(u'wanted').document(u'company').set(data)
