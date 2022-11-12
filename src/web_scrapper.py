50fd570db01427b03f7d429d8383d0fc8c124968
0nmyhead <jg020135@naver.com>
2022-11-12 15:41:36 +0900

    handled refresh conflict fixed by using selenium

--- src/web_scrapper.py
+++ src/web_scrapper.py
@@ -1,16 +1,32 @@
 import re
 import requests
+import time
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
-def get_page(page_url):
+def get_pagelist(page_url):
     
     """
     """
+    
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(page_url)
+    driver.implicitly_wait(5)
+    driver.find_element_by_xpath('//*[@id="__next"]/div[3]/div/div/div[1]/div[1]/div/div[2]/div').click()
+    driver.implicitly_wait(5)
+    driver.find_element_by_xpath('//*[@id="__next"]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div[1]/ul/li[1]/button').click()
+    driver.implicitly_wait(10)
+    
+    counter = 0
+    
+    while counter < 5 :
+        
+        driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+        time.sleep(3)
+        counter += 1
 
-    page = requests.get(page_url)
-    page.raise_for_status()
-
-    soup = BeautifulSoup(page.text, 'html.parser')
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     
     items = soup.select("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li")
     pagelist = []
@@ -18,12 +34,17 @@ def get_page(page_url):
     for item in items:
         
         pagenum = item.select_one("#__next > div.JobList_cn__t_THp > div > div > div.List_List_container__JnQMS > ul > li > div > a")["href"]
-        print(pagenum)
+        pagelist.append(pagenum)
     
+    driver.quit()
+    
+    return pagelist
+
+def get_info(page_url):
     
     return 0
     
 
 url = 'https://www.wanted.co.kr/wdlist/518/655?country=kr&job_sort=job.latest_order&years=-1&locations=all'
 
-plist = get_page(url)
+plist = get_pagelist(url)
